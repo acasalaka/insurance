7de334ef214c5ce19f3932e0628fab15a967eec9
RizvanuSN <rizvanusn@gmail.com>
2024-10-07 16:07:03 +0700

    docs: add README.md file

---
+++ insurance2206823682/src/main/java/apap/ti/insurance2206823682/Insurance2206823682Application.java
@@ -0,0 +1,13 @@
+package apap.ti.insurance2206823682;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Insurance2206823682Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Insurance2206823682Application.class, args);
+	}
+
+}
---
+++ insurance2206823682/src/test/java/apap/ti/insurance2206823682/Insurance2206823682ApplicationTests.java
@@ -0,0 +1,13 @@
+package apap.ti.insurance2206823682;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Insurance2206823682ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
