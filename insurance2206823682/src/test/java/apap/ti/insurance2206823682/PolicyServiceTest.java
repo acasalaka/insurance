ab159c2c4b8affa9cb7d568f9ff79193eba5929a
RizvanuSN <rizvanusn@gmail.com>
2024-10-20 18:06:29 +0700

    test: fix test file

--- insurance2206823682/src/test/java/apap/ti/insurance2206823682/PolicyServiceTest.java
+++ insurance2206823682/src/test/java/apap/ti/insurance2206823682/PolicyServiceTest.java
@@ -28,7 +28,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
-public class PolicyControllerTest {
+public class PolicyServiceTest {
 
     private MockMvc mockMvc;
 
